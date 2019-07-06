4628d14e9ac68d6bde3a964731c8cbbb32774611
cyclingscotty <46350366+cyclingscotty@users.noreply.github.com>
2019-07-06 10:06:40 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -12,96 +12,6 @@ const numbers = [
 ];
 
 const words = {
-    "frog": [
-    "frog",
-    "frogs",
-    "images/frog.png"
-],
-    "horse":[
-        "horse",
-        "horses",
-        "images/horse.png"
-    ],
-    "fish":[
-        "fish",
-        "fish",
-        "images/fish.png"
-    ],
-    "sheep" :[
-        "sheep",
-        "sheep",
-        "images/sheep.png"
-    ],
-    "lizard" :[
-        "lizard",
-        "lizards",
-        "images/lizard.png"
-    ],
-    snake:[
-        "snake",
-        "snakes",
-        "images/snake.png"
-    ],
-    cow:[
-        "cow",
-        "cows",
-        "images/cow.png"
-    ],
-    turtle:[
-        "turtle",
-        "turtles",
-        "images/turtle.png"
-    ],
-    snail:[
-        "snail",
-        "snails",
-        "images/snail.png"
-    ],
-    spider:[
-        "spider",
-        "spiders",
-        "images/spider.png"
-    ],
-    ladyBug:[
-        "ladybug",
-        "ladybugs",
-        "images/ladybug.png"
-    ],
-    worm:[
-        "worm",
-        "worms",
-        "images/worm.png"
-    ],
-    duck:[
-        "duck",
-        "ducks",
-        "images/duck.png"
-    ],
-    elephant:[
-        "elephant",
-        "elephants",
-        "images/elephant.png"
-    ],
-    lion:[
-        "lion",
-        "lions",
-        "images/lion.png"
-    ],
-    tiger:[
-        "tiger",
-        "tigers",
-        "images/tiger.png"
-    ],
-    bear:[
-        "bear",
-        "bears",
-        "images/bear.png"
-    ],
-    bird:[
-        "bird",
-        "birds",
-        "images/bird.png"
-    ],
     dog:[
         "dog",
         "dogs",
@@ -117,6 +27,111 @@ const words = {
         "monkeys",
         "images/monkey.png"
     ],
+    duck:[
+        "duck",
+        "ducks",
+        "images/duck.png"
+    ],
+    pig:[
+        "pig",
+        "pigs",
+        "images/pig.png"
+    ],
+     cow:[
+        "cow",
+        "cows",
+        "images/cow.png"
+    ],
+    frog: [
+    "frog",
+    "frogs",
+    "images/frog.png"
+],
+    horse:[
+        "horse",
+        "horses",
+        "images/horse.png"
+    ],
+    fish:[
+        "fish",
+        "fish",
+        "images/fish.png"
+    ],
+    bird:[
+        "bird",
+        "birds",
+        "images/bird.png"
+    ],
+    
+  //10  
+    
+    
+    zebra:[
+        "zebra",
+        "zebras",
+        "images/zebra.png"
+    ],
+    deer:[
+        "deer",
+        "deer",
+        "images/deer.png"
+    ],
+    hippo:[
+        "hippo",
+        "hippos",
+        "images/hippo.png"
+    ],
+    spider:[
+        "spider",
+        "spiders",
+        "images/spider.png"
+    ],
+    "sheep" :[
+        "sheep",
+        "sheep",
+        "images/sheep.png"
+    ],
+    
+    snake:[
+        "snake",
+        "snakes",
+        "images/snake.png"
+    ],
+   
+    turtle:[
+        "turtle",
+        "turtles",
+        "images/turtle.png"
+    ],
+    snail:[
+        "snail",
+        "snails",
+        "images/snail.png"
+    ],
+    elephant:[
+        "elephant",
+        "elephants",
+        "images/elephant.png"
+    ],
+    tiger:[
+        "tiger",
+        "tigers",
+        "images/tiger.png"
+    ],
+    
+    //20
+    
+    
+    lion:[
+        "lion",
+        "lions",
+        "images/lion.png"
+    ],
+    bear:[
+        "bear",
+        "bears",
+        "images/bear.png"
+    ],
     gorilla:[
         "gorilla",
         "gorillas",
@@ -126,16 +141,6 @@ const words = {
         "kangaroo",
         "kangaroos",
         "images/kangaroo.png"
-    ],
-    octopus:[
-        "octopus",
-        "octopuses",
-        "images/octopus.png"
-    ],
-    pig:[
-        "pig",
-        "pigs",
-        "images/pig.png"
     ],
     goat:[
         "goat",
@@ -147,23 +152,96 @@ const words = {
         "koalas",
         "images/koala.png"
     ],
-    koala:[
+    panda:[
         "panda",
         "pandas",
         "images/panda.png"
+    ],
+    giraffe:[
+        "giraffe",
+        "giraffes",
+        "images/giraffe.png"
+    ],
+    
+    chicken:[
+        "chicken",
+        "chickens",
+        "images/chicken.png"
+    ],
+    penguin:[
+        "penguin",
+        "penguins",
+        "images/penguin.png"
+    ],
+    //30
+    
+    
+    
+    camel:[
+        "camel",
+        "camels",
+        "images/camel.png"
+    ],
+    crocodile:[
+        "crocodile",
+        "crocodiles",
+        "images/crocodile.png"
+    ],
+    worm:[
+        "worm",
+        "worms",
+        "images/worm.png"
+    ],
+    ladyBug:[
+        "ladybug",
+        "ladybugs",
+        "images/ladybug.png"
+    ],
+    octopus:[
+        "octopus",
+        "octopuses",
+        "images/octopus.png"
+    ],
+    lizard :[
+        "lizard",
+        "lizards",
+        "images/lizard.png"
+    ],
+    rhino :[
+        "rhino",
+        "rhinos",
+        "images/rhino.png"
+    ],
+    whale :[
+        "whale",
+        "whales",
+        "images/whale.png"
+    ],
+    flamingo :[
+        "flamingo",
+        "flamingoes",
+        "images/flamingo.png"
     ]
+    
 };
 
-function randomNumber(){
+
+
+
+function randomNumber(){    
+    
     let howMany = Math.floor(Math.random()*10);
-  document.getElementById('number').innerHTML = numbers[howMany] + " <button id='numberButton'>Go!</button>";
-  document.getElementById('numberButton').addEventListener('click', function(){
-    randomWord(howMany);
-       });
+  document.getElementById('number').innerHTML = numbers[howMany]; // + " <button id='numberButton'>Go!</button>";
+  //document.getElementById('numberButton').addEventListener('click', function(){
+   randomWord(howMany);
+   //    });
 }
 
 function randomWord(howMany){
+    
+    
     let theWord = Math.floor(Math.random()* Object.keys(words).length);
+    
     let theObject = Object.keys(words)[theWord];
     console.log(theWord);
     console.log(theObject);
